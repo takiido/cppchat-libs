6a213af52e8b1d1687db1e297677c4e32cccc609
Alex Tovshchyk <157808651+takiido@users.noreply.github.com>
2025-05-16 16:32:50 -0500

    Fix

--- setup-libclang.sh
+++ setup-libclang.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -e
 
-cd "$(dirname "$0")/libclang"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ZIP_PATH="$ROOT_DIR/libclang/lib.zip"
+UNZIP_TARGET_DIR="$ROOT_DIR/libclang/lib"
+SO_FILE="$UNZIP_TARGET_DIR/libclang.so"
 
-if [ ! -f "lib/libclang.so" ]; then
-  echo "📦 Extracting libclang/libclang-lib.zip..."
-  unzip -q libclang-lib.zip
-else
-  echo "✅ libclang already unpacked."
+if [ -f "$SO_FILE" ]; then
+  echo "✅ libclang already unpacked at $SO_FILE"
+  exit 0
 fi
+
+if [ ! -f "$ZIP_PATH" ]; then
+  echo "❌ lib.zip not found at $ZIP_PATH"
+  exit 1
+fi
+
+echo "📦 Extracting libclang/lib.zip..."
+unzip -q "$ZIP_PATH" -d "$ROOT_DIR/libclang"
+echo "✅ Extraction complete: $SO_FILE"
